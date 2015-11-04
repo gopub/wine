84fcb2885285e289cc19092a74e223ca29ec41cd
儒丁 <qiyu.tqy@alibaba-inc.com>
2015-11-04 16:31:13 +0800

    add hello_wine

---
+++ examples/hello_wine.go
@@ -0,0 +1,10 @@
+package main
+import "github.com/justintan/wine"
+
+func main()  {
+	s := wine.Server()
+	s.GP("/", func(c wine.Context) {
+		c.SendHTML("Hello, This is WINE!")
+	})
+	s.Run(":8001")
+}
