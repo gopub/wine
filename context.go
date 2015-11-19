921fb93616fe6ee26747be4c33074464ea41338e
儒丁 <qiyu.tqy@alibaba-inc.com>
2015-11-19 22:54:03 +0800

    simplify Context

--- context.go
+++ context.go
@@ -13,16 +13,14 @@ type Context interface {
 	Get(key string) interface{}
 	Written() bool
 	MarkWritten()
-	HandlerChain() *handlerChain
-	Templates() []*template.Template
 	Next()
 	HttpRequest() *http.Request
 	RequestParams() gox.M
 	RequestHeader() http.Header
 	ResponseHeader() http.Header
 	ResponseWriter() http.ResponseWriter
-	SendJSON(obj interface{})
 	SendStatus(status int)
+	SendJSON(obj interface{})
 	SendFile(filePath string)
 	SendHTML(htmlText string)
 	SendTemplateHTML(templateFileName string, params gox.M)
