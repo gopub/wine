1718f25f3b2661de5caf537f6b825de0e89c906d
justin <qiyutan@gmail.com>
2016-03-31 22:52:04 +0800

    examples

--- examples/services/hello/main.go
+++ examples/services/hello/main.go
@@ -1,1 +1,11 @@
-package hello
+package main
+
+import "github.com/justintan/wine"
+
+func main() {
+	s := wine.Default()
+	s.Get("hello", func(c wine.Context) {
+		c.Text("Hello, wine!")
+	})
+	s.Run(":8000")
+}
--- examples/websites/hello/main.go
+++ examples/websites/hello/main.go
@@ -1,1 +1,14 @@
-package hello
+package main
+
+/*
+ * Enter directory examples/websites/hello
+ * go run ./main.go
+ */
+
+import "github.com/justintan/wine"
+
+func main() {
+	s := wine.Default()
+	s.StaticDir("/", "./html")
+	s.Run(":8000")
+}
