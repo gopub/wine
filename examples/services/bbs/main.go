92c0ae614d0b29fcb36c40f6be689cffc2f6a32a
justin <qiyutan@gmail.com>
2016-07-13 10:02:50 +0800

    add bbs

---
+++ examples/services/bbs/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"github.com/justintan/gox/log"
+	"github.com/justintan/gox/types"
+	"github.com/justintan/wine"
+	"net/http"
+	_ "net/http/pprof"
+	"time"
+)
+
+type User struct {
+	ID        types.ID `json:"id"`
+	Name      string   `json:"name"`
+	CreatedAt int64    `json:"created_at"`
+}
+
+type Topic struct {
+	ID        types.ID `json:"id"`
+	User      *User    `json:"user"`
+	Title     string   `json:"title"`
+	CreatedAt int64    `json:"created_at"`
+}
+
+func main() {
+	s := wine.Default()
+	s.Get("login", func(c wine.Context) {
+		username := c.Params().GetStr("username")
+		password := c.Params().GetStr("password")
+		if len(username) == 0 || len(password) == 0 {
+			c.JSON(types.M{"code": 1, "msg": "login error"})
+			return
+		}
+		u := &User{}
+		u.ID = 1
+		u.Name = "guest"
+		u.CreatedAt = time.Now().Unix()
+		c.JSON(types.M{"code": 0, "msg": "success", "data": u})
+	})
+
+	s.Post("topic", func(c wine.Context) {
+		title := c.Params().GetStr("title")
+		if len(title) == 0 {
+			c.JSON(types.M{"code": 1, "msg": "no title"})
+			return
+		}
+		t := &Topic{}
+		t.ID = 2
+		t.User = &User{ID: 1, Name: "guest", CreatedAt: time.Now().Unix()}
+		t.CreatedAt = time.Now().Unix()
+		c.JSON(types.M{"code": 2, "msg": "success", "data": t})
+	})
+
+	go func() {
+		log.Debug(http.ListenAndServe("localhost:6060", nil))
+	}()
+
+	s.Run(":8000")
+}
