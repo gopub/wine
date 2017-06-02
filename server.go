d1f76b8f166ade9d9697276b294416151c0042c4
justin <qiyutan@gmail.com>
2017-06-02 16:33:32 +0800

    Add comments and AddTemplateFuncs

--- server.go
+++ server.go
@@ -2,23 +2,26 @@ package wine
 
 import (
 	"context"
-	"github.com/justintan/gox/runtime"
 	"html/template"
 	"log"
 	"net/http"
 	"strings"
 	"sync"
+
+	"github.com/justintan/gox/runtime"
 )
 
 type Server struct {
 	Router
-	Header      http.Header
-	context     Context
-	templates   []*template.Template
-	contextPool sync.Pool
-	server      *http.Server
+	Header        http.Header
+	context       Context
+	templates     []*template.Template
+	templateFuncs template.FuncMap
+	contextPool   sync.Pool
+	server        *http.Server
 }
 
+// NewServer returns a server
 func NewServer() *Server {
 	s := &Server{}
 	s.Router = NewDefaultRouter()
@@ -30,12 +33,14 @@ func NewServer() *Server {
 	return s
 }
 
+// Default returns a default server with Logger interceptor
 func Default() *Server {
 	s := NewServer()
 	s.Use(Logger)
 	return s
 }
 
+// RegisterContext registers a Context implementation
 func (s *Server) RegisterContext(c Context) {
 	if c == nil {
 		panic("[WINE] c is nil")
@@ -49,6 +54,7 @@ func (s *Server) newContext() interface{} {
 	return c
 }
 
+// Run starts server
 func (s *Server) Run(addr string) error {
 	if s.server != nil {
 		panic("[WINE] Server is running")
@@ -64,10 +70,12 @@ func (s *Server) Run(addr string) error {
 	return err
 }
 
+// Shutdown stops server
 func (s *Server) Shutdown() {
 	s.server.Shutdown(context.Background())
 }
 
+// ServeHTTP implements for http.Handler interface, which will handle each http request
 func (s *Server) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	defer func() {
 		if e := recover(); e != nil {
@@ -109,16 +117,41 @@ func (s *Server) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	s.contextPool.Put(c)
 }
 
+// AddGlobTemplate adds a template by parsing template files with pattern
 func (s *Server) AddGlobTemplate(pattern string) {
 	tpl := template.Must(template.ParseGlob(pattern))
 	s.AddTemplate(tpl)
 }
 
+// AddFilesTemplate adds a template by parsing template files
 func (s *Server) AddFilesTemplate(files ...string) {
 	tpl := template.Must(template.ParseFiles(files...))
 	s.AddTemplate(tpl)
 }
 
+// AddTemplate adds a template
 func (s *Server) AddTemplate(tpl *template.Template) {
+	if s.templateFuncs != nil {
+		tpl.Funcs(s.templateFuncs)
+	}
 	s.templates = append(s.templates, tpl)
+}
+
+// AddTemplateFuncs adds template functions
+func (s *Server) AddTemplateFuncs(funcs template.FuncMap) {
+	if funcs == nil {
+		return
+	}
+
+	if s.templateFuncs == nil {
+		s.templateFuncs = funcs
+	} else {
+		for name, f := range funcs {
+			s.templateFuncs[name] = f
+		}
+	}
+
+	for _, tpl := range s.templates {
+		tpl.Funcs(funcs)
+	}
 }
