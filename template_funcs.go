f99c65da9150826c3caa8fafc144df21dbd25e41
justin <qiyutan@gmail.com>
2017-06-02 16:47:12 +0800

    Add arithmetic template functions

---
+++ template_funcs.go
@@ -0,0 +1,17 @@
+package wine
+
+func plus(a, b int) int {
+	return a + b
+}
+
+func minus(a, b int) int {
+	return a - b
+}
+
+func multiple(a, b int) int {
+	return a * b
+}
+
+func divide(a, b int) int {
+	return a / b
+}
