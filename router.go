1cf467539b9baeefef5cca4ec9e5ff9288cddf4e
justin <qiyutan@gmail.com>
2017-06-03 23:14:39 +0800

    add comments

--- router.go
+++ router.go
@@ -5,10 +5,11 @@ import (
 	"strings"
 )
 
+// Router routes path to handlers
 type Router interface {
 	Group(path string) Router
 	UseHandlers(handlers ...Handler) Router
-	Use(funcList ...HandlerFunc) Router
+	Use(funcs ...HandlerFunc) Router
 	StaticFile(path, filePath string)
 	StaticDir(path, filePath string)
 	StaticFS(path string, fs http.FileSystem)
@@ -28,19 +29,22 @@ type Router interface {
 	Any(path string, handlers ...HandlerFunc)
 }
 
+// DefaultRouter is a default implementation of Router interface
 type DefaultRouter struct {
 	methodTrees map[string]*node
 	basePath    string
 	handlers    []Handler
 }
 
+// NewDefaultRouter new a DefaultRouter
 func NewDefaultRouter() *DefaultRouter {
 	r := &DefaultRouter{}
 	r.methodTrees = make(map[string]*node, 4)
 	return r
 }
 
-func (dr *DefaultRouter) Group(path string) Router {
+// Group returns a new router whose basePath is r.basePath+path
+func (r *DefaultRouter) Group(path string) Router {
 	if len(path) == 0 {
 		panic("[WINE] group path is empty")
 	}
@@ -49,31 +53,35 @@ func (dr *DefaultRouter) Group(path string) Router {
 		panic("[WINE] don't create default group \"/\"")
 	}
 
-	r := &DefaultRouter{}
-	r.methodTrees = dr.methodTrees
-	r.basePath = normalizePath(dr.basePath + "/" + path)
-	r.handlers = make([]Handler, len(dr.handlers))
-	copy(r.handlers, dr.handlers)
+	nr := &DefaultRouter{}
+	nr.methodTrees = r.methodTrees
+	nr.basePath = normalizePath(r.basePath + "/" + path)
+	nr.handlers = make([]Handler, len(r.handlers))
+	copy(nr.handlers, r.handlers)
+	return nr
+}
+
+// UseHandlers returns a new router with global handlers which will be bound with all new path patterns
+// This can be used to add interceptors
+func (r *DefaultRouter) UseHandlers(handlers ...Handler) Router {
+	if len(handlers) == 0 {
+		return r
+	}
+	r.handlers = append(r.handlers, handlers...)
 	return r
 }
 
-func (dr *DefaultRouter) UseHandlers(handlers ...Handler) Router {
-	if len(handlers) == 0 {
-		return dr
+// Use is similar with UseHandlers
+func (r *DefaultRouter) Use(funcs ...HandlerFunc) Router {
+	if len(funcs) == 0 {
+		return r
 	}
-	dr.handlers = append(dr.handlers, handlers...)
-	return dr
+	return r.UseHandlers(convertToHandlers(funcs...)...)
 }
 
-func (dr *DefaultRouter) Use(funcList ...HandlerFunc) Router {
-	if len(funcList) == 0 {
-		return dr
-	}
-	return dr.UseHandlers(convertToHandlers(funcList...)...)
-}
-
-func (dr *DefaultRouter) Match(method string, path string) (handlers []Handler, params map[string]string) {
-	n := dr.methodTrees[method]
+// Match finds handlers and parses path parameters according to method and path
+func (r *DefaultRouter) Match(method string, path string) (handlers []Handler, params map[string]string) {
+	n := r.methodTrees[method]
 	if n == nil {
 		return
 	}
@@ -85,7 +93,8 @@ func (dr *DefaultRouter) Match(method string, path string) (handlers []Handler, 
 	return n.match(segments)
 }
 
-func (dr *DefaultRouter) Bind(method string, path string, handlers ...Handler) {
+// Bind binds method, path with handlers
+func (r *DefaultRouter) Bind(method string, path string, handlers ...Handler) {
 	if path == "" {
 		panic("[WINE] invalid path")
 	}
@@ -98,18 +107,18 @@ func (dr *DefaultRouter) Bind(method string, path string, handlers ...Handler) {
 		panic("[WINE] no handler")
 	}
 
-	n := dr.methodTrees[method]
+	n := r.methodTrees[method]
 	if n == nil {
 		n = &node{}
 		n.t = staticNode
-		dr.methodTrees[method] = n
+		r.methodTrees[method] = n
 	}
 
-	hs := make([]Handler, len(dr.handlers))
-	copy(hs, dr.handlers)
+	hs := make([]Handler, len(r.handlers))
+	copy(hs, r.handlers)
 	hs = append(hs, handlers...)
 
-	path = normalizePath(dr.basePath + "/" + path)
+	path = normalizePath(r.basePath + "/" + path)
 	if path == "" {
 		if len(n.handlers) == 0 {
 			n.handlers = hs
@@ -124,20 +133,23 @@ func (dr *DefaultRouter) Bind(method string, path string, handlers ...Handler) {
 	}
 }
 
-func (dr *DefaultRouter) StaticFile(path, filePath string) {
-	dr.Get(path, func(c Context) {
+// StaticFile binds path to a file
+func (r *DefaultRouter) StaticFile(path, filePath string) {
+	r.Get(path, func(c Context) {
 		c.File(filePath)
 	})
 	return
 }
 
-func (dr *DefaultRouter) StaticDir(path, dirPath string) {
-	dr.StaticFS(path, http.Dir(dirPath))
+// StaticDir binds path to a directory
+func (r *DefaultRouter) StaticDir(path, dirPath string) {
+	r.StaticFS(path, http.Dir(dirPath))
 	return
 }
 
-func (dr *DefaultRouter) StaticFS(path string, fs http.FileSystem) {
-	prefix := normalizePath(dr.basePath + "/" + path)
+// StaticFS binds path to an abstract file system
+func (r *DefaultRouter) StaticFS(path string, fs http.FileSystem) {
+	prefix := normalizePath(r.basePath + "/" + path)
 	if len(prefix) == 0 {
 		prefix = "/"
 	} else if prefix[0] != '/' {
@@ -156,61 +168,61 @@ func (dr *DefaultRouter) StaticFS(path string, fs http.FileSystem) {
 	}
 
 	fileServer := http.StripPrefix(prefix, http.FileServer(fs))
-	dr.Get(path, func(c Context) {
+	r.Get(path, func(c Context) {
 		c.ServeHTTP(fileServer)
 	})
 	return
 }
 
-func (dr *DefaultRouter) HandleGet(path string, handlers ...Handler) {
-	dr.Bind("GET", path, handlers...)
+func (r *DefaultRouter) HandleGet(path string, handlers ...Handler) {
+	r.Bind("GET", path, handlers...)
 	return
 }
 
-func (dr *DefaultRouter) HandlePost(path string, handlers ...Handler) {
-	dr.Bind("POST", path, handlers...)
+func (r *DefaultRouter) HandlePost(path string, handlers ...Handler) {
+	r.Bind("POST", path, handlers...)
 	return
 }
 
-func (dr *DefaultRouter) HandlePut(path string, handlers ...Handler) {
-	dr.Bind("PUT", path, handlers...)
+func (r *DefaultRouter) HandlePut(path string, handlers ...Handler) {
+	r.Bind("PUT", path, handlers...)
 	return
 }
 
-func (dr *DefaultRouter) HandleDelete(path string, handlers ...Handler) {
-	dr.Bind("DELETE", path, handlers...)
+func (r *DefaultRouter) HandleDelete(path string, handlers ...Handler) {
+	r.Bind("DELETE", path, handlers...)
 	return
 }
 
-func (dr *DefaultRouter) HandleAny(path string, handlers ...Handler) {
-	dr.HandleGet(path, handlers...)
-	dr.HandlePost(path, handlers...)
-	dr.HandlePut(path, handlers...)
+func (r *DefaultRouter) HandleAny(path string, handlers ...Handler) {
+	r.HandleGet(path, handlers...)
+	r.HandlePost(path, handlers...)
+	r.HandlePut(path, handlers...)
 	return
 }
 
-func (dr *DefaultRouter) Get(path string, funcList ...HandlerFunc) {
-	dr.HandleGet(path, convertToHandlers(funcList...)...)
+func (r *DefaultRouter) Get(path string, funcs ...HandlerFunc) {
+	r.HandleGet(path, convertToHandlers(funcs...)...)
 }
 
-func (dr *DefaultRouter) Post(path string, funcList ...HandlerFunc) {
-	dr.HandlePost(path, convertToHandlers(funcList...)...)
+func (r *DefaultRouter) Post(path string, funcs ...HandlerFunc) {
+	r.HandlePost(path, convertToHandlers(funcs...)...)
 }
 
-func (dr *DefaultRouter) Put(path string, funcList ...HandlerFunc) {
-	dr.HandlePut(path, convertToHandlers(funcList...)...)
+func (r *DefaultRouter) Put(path string, funcs ...HandlerFunc) {
+	r.HandlePut(path, convertToHandlers(funcs...)...)
 }
 
-func (dr *DefaultRouter) Delete(path string, funcList ...HandlerFunc) {
-	dr.HandleDelete(path, convertToHandlers(funcList...)...)
+func (r *DefaultRouter) Delete(path string, funcs ...HandlerFunc) {
+	r.HandleDelete(path, convertToHandlers(funcs...)...)
 }
 
-func (dr *DefaultRouter) Any(path string, funcList ...HandlerFunc) {
-	dr.HandleAny(path, convertToHandlers(funcList...)...)
+func (r *DefaultRouter) Any(path string, funcs ...HandlerFunc) {
+	r.HandleAny(path, convertToHandlers(funcs...)...)
 }
 
-func (dr *DefaultRouter) Print() {
-	for m, n := range dr.methodTrees {
+func (r *DefaultRouter) Print() {
+	for m, n := range r.methodTrees {
 		n.Print(m, "/")
 	}
 }
