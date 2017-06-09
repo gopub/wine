b431a026f65b00e1f08fbd349a24331936d53e9b
justin <qiyutan@gmail.com>
2017-06-09 17:51:54 +0800

    Support basic auth

---
+++ basic_auth.go
@@ -0,0 +1,46 @@
+package wine
+
+import (
+	"encoding/base64"
+	"net/http"
+	"strconv"
+)
+
+// BasicAuthUser is key for basic auth user
+const KeyBasicAuthUser = "basic_auth_user"
+
+// BasicAuth returns a basic auth interceptor
+func BasicAuth(userToPassword map[string]string, realm string) HandlerFunc {
+	if len(userToPassword) == 0 {
+		panic("userToPassword is empty")
+	}
+
+	userToAuthInfo := make(map[string]string)
+	for user, password := range userToPassword {
+		if len(user) == 0 || len(password) == 0 {
+			panic("Empty user or password")
+		}
+		info := user + ":" + password
+		userToAuthInfo[user] = "Basic " + base64.StdEncoding.EncodeToString([]byte(info))
+	}
+
+	authHeaderValue := "Basic realm=" + strconv.Quote(realm)
+	return func(c Context) {
+		authValue := c.Header().Get("Authorization")
+		var foundUser string
+		for user, info := range userToAuthInfo {
+			if info == authValue {
+				foundUser = user
+				break
+			}
+		}
+
+		if len(foundUser) == 0 {
+			c.ResponseHeader().Set("WWW-Authenticate", authHeaderValue)
+			c.Status(http.StatusUnauthorized)
+		} else {
+			c.Set(KeyBasicAuthUser, foundUser)
+			c.Next()
+		}
+	}
+}
