0a8899d86b45d3e0d5bca2266e79b9ae9191742f
justin <qiyutan@gmail.com>
2017-06-15 09:26:05 +0800

    add test case

--- server_test.go
+++ server_test.go
@@ -2,6 +2,7 @@ package wine
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -56,5 +57,96 @@ func TestJSON(t *testing.T) {
 
 	if resp.Header["Content-Type"][0] != "application/json; charset=utf-8" {
 		t.Fatal(resp.Header["Content-Type"])
+	}
+}
+
+func TestHTML(t *testing.T) {
+	var htmlText = `
+	<html>
+		<header>
+		</header>
+		<body>
+			Hello, world!
+		</body>
+	</html>
+	`
+	_testServer.Get("/html/hello.html", func(c Context) {
+		c.HTML(htmlText)
+	})
+
+	resp, err := http.DefaultClient.Get("http://localhost:8000/html/hello.html")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp.Body.Close()
+	if string(data) != htmlText {
+		t.Fatal(string(data))
+	}
+
+	if resp.Header["Content-Type"][0] != "text/html; charset=utf-8" {
+		t.Fatal(resp.Header["Content-Type"])
+	}
+}
+
+func TestPathParams(t *testing.T) {
+	_testServer.Get("/sum/:a,:b", func(c Context) {
+		a := c.Params().Int("a")
+		b := c.Params().Int("b")
+		c.Text(fmt.Sprint(a + b))
+	})
+
+	_testServer.Get("/sum/:a,:b,:c", func(c Context) {
+		a := c.Params().Int("a")
+		b := c.Params().Int("b")
+		cc := c.Params().Int("c")
+		c.Text(fmt.Sprint(a + b + cc))
+	})
+
+	{
+		resp, err := http.DefaultClient.Get("http://localhost:8000/sum/1,2")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		resp.Body.Close()
+		if string(data) != "3" {
+			t.Fatal(string(data))
+		}
+	}
+
+	{
+		resp, err := http.DefaultClient.Get("http://localhost:8000/sum/1,2,-9")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		resp.Body.Close()
+		if string(data) != "-6" {
+			t.Fatal(string(data))
+		}
+	}
+
+	{
+		resp, err := http.DefaultClient.Get("http://localhost:8000/sum/1")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if resp.StatusCode != http.StatusNotFound {
+			t.Fatal(resp.Status)
+		}
 	}
 }
