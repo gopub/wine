2c831de613c390a30df08a5d6615199873c37cf2
Justin Tan <justin@driver.xyz>
2018-09-08 18:29:05 +0800

    Remove log

--- server.go
+++ server.go
@@ -147,13 +147,9 @@ func (s *Server) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	c := s.makeContext(rw, req, handlers)
 	reqCtx, cancel := context.WithTimeout(req.Context(), s.RequestTimeout)
 	defer cancel()
-	log.Info("1")
 	c.req = req.WithContext(reqCtx)
-	log.Info("2")
 	c.Params().AddMapObj(pathParams)
-	log.Info("3")
 	c.Next()
-	log.Info("4")
 	if !c.Responded() {
 		c.Status(http.StatusNotFound)
 	}
@@ -210,8 +206,6 @@ func (s *Server) AddTemplateFuncMap(funcMap template.FuncMap) {
 }
 
 func (s *Server) makeContext(rw http.ResponseWriter, req *http.Request, handlers []Handler) *Context {
-
-	log.Debug("START")
 	c := s.contextPool.Get().(*Context)
 	c.reqParams = utils.ParseHTTPRequestParameters(req, s.MaxRequestMemory)
 	for k := range c.keyValues {
@@ -231,6 +225,5 @@ func (s *Server) makeContext(rw http.ResponseWriter, req *http.Request, handlers
 		c.Responder.Header()[k] = v
 	}
 
-	log.Debug("DONE")
 	return c
 }
