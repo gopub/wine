18569cec06a083808cd229dbda1c6d9139152293
JustinTan <mrnatande@gmail.com>
2019-01-09 19:06:23 +0800

    Support setting value of alias type in redis

--- sessions/redis_store.go
+++ sessions/redis_store.go
@@ -1,7 +1,10 @@
 package sessions
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/go-redis/redis"
+	"reflect"
 	"time"
 )
 
@@ -29,11 +32,90 @@ func NewRedisStoreWithClient(client *redis.Client) *RedisStore {
 
 func (s *RedisStore) Get(sid, key string, ptrValue interface{}) error {
 	cmd := s.client.HGet(sid, key)
-	return cmd.Scan(ptrValue)
+
+	pv := reflect.ValueOf(ptrValue)
+	if pv.Kind() != reflect.Ptr {
+		panic("ptrValue must be a pointer")
+	}
+
+	if b, ok := ptrValue.(*[]byte); ok {
+		data, err := cmd.Bytes()
+		if err != nil {
+			return err
+		}
+		*b = data
+		return nil
+	}
+
+	switch v := pv.Elem(); v.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		i, err := cmd.Int64()
+		if err != nil {
+			return err
+		}
+		v.SetInt(i)
+		return nil
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		i, err := cmd.Uint64()
+		if err != nil {
+			return err
+		}
+		v.SetUint(i)
+		return nil
+	case reflect.Float32, reflect.Float64:
+		i, err := cmd.Float64()
+		if err != nil {
+			return err
+		}
+		v.SetFloat(i)
+		return nil
+	case reflect.String:
+		i, err := cmd.Result()
+		if err != nil {
+			return err
+		}
+		v.SetString(i)
+		return nil
+	default:
+		data, _ := cmd.Bytes()
+		return json.Unmarshal(data, ptrValue)
+	}
 }
 
 func (s *RedisStore) Set(sid, key string, value interface{}) error {
-	return s.client.HSet(sid, key, value).Err()
+	switch v := reflect.ValueOf(value); v.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		fallthrough
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		fallthrough
+	case reflect.Float32, reflect.Float64:
+		fallthrough
+	case reflect.String:
+		err := s.client.HSet(sid, key, fmt.Sprint(value)).Err()
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	if _, ok := value.([]byte); ok {
+		err := s.client.HSet(sid, key, value).Err()
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	b, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+
+	err = s.client.HSet(sid, key, b).Err()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (s *RedisStore) Delete(sid string) error {
