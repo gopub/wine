c31f20883a8fbc9af201752f5255fa26cc37b979
JustinTan <mrnatande@gmail.com>
2019-11-16 16:59:51 +0800

    Allow parsing non json request

--- api/request.go
+++ api/request.go
@@ -10,8 +10,16 @@ import (
 
 // ParseRequest unmarshal request body into params. Only support JSON type for now.
 func ParseParams(req *wine.Request, params interface{}) error {
-	if req.ContentType() != mime.JSON {
-		return gox.BadRequest("Expected %s instead of %s", mime.JSON, req.ContentType())
+	if req.ContentType() == mime.JSON {
+		err := json.Unmarshal(req.Body(), params)
+		if err != nil {
+			return gox.BadRequest("unmarshal: %v", err)
+		}
+		return nil
 	}
-	return json.Unmarshal(req.Body(), params)
+	err := gox.CopyWithNamer(params, req.Params(), gox.SnakeToCamelNamer)
+	if err != nil {
+		return gox.BadRequest("parse params: %v", err)
+	}
+	return nil
 }
