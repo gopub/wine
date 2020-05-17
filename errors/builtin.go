45ba7402e317b960a2a523c772a372247a03de24
JustinTan <mrnatande@gmail.com>
2020-05-17 10:01:12 +0800

    Add errors

---
+++ errors/builtin.go
@@ -0,0 +1,19 @@
+package errors
+
+import "errors"
+
+func New(text string) error {
+	return errors.New(text)
+}
+
+func Unwrap(err error) error {
+	return errors.Unwrap(err)
+}
+
+func Is(err, target error) bool {
+	return errors.Is(err, target)
+}
+
+func As(err error, target interface{}) bool {
+	return errors.As(err, target)
+}
