6dc375df21cead14d8ca85be50f640b6430c7269
nat <mrnatande@gmail.com>
2020-12-08 20:48:07 -0500

    Import new package

--- exp/stream/byte_test.go
+++ exp/stream/byte_test.go
@@ -9,7 +9,7 @@ import (
 	"time"
 
 	"github.com/gopub/wine"
-	"github.com/gopub/wine/plugin/stream"
+	"github.com/gopub/wine/exp/stream"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
--- exp/stream/text_test.go
+++ exp/stream/text_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 
 	"github.com/gopub/wine"
-	"github.com/gopub/wine/plugin/stream"
+	"github.com/gopub/wine/exp/stream"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
